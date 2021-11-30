a7ece0724f364e40c7d1dc2ffc5a3547c03a44b1
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2021-11-30 17:33:02 +0100

    Now builds in Visual Studio Win 11

--- day0/part1.cpp
+++ day0/part1.cpp
@@ -5,6 +5,8 @@
 int main(int argc, char *argv[])
 {
     std::cout << "\n" << "Hello :)";
+    std::cout << "\nPress any key...";
+    std::cin.get();
     std::cout << "\n" << "Bye!";
     std::cout << "\n";
     return 0;
