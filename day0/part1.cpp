be5d1d3ab6d3ffecf37d9f10bd93b52f87b9448e
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2021-11-30 16:44:55 +0100

    Builds in VSCode macOS

---
+++ day0/part1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+ 
+int main(int argc, char *argv[])
+{
+    std::cout << "\n" << "Hello :)";
+    std::cout << "\n" << "Bye!";
+    std::cout << "\n";
+    return 0;
+}
