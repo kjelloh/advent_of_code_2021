6177de278124476f4773337c897e1015d275945e
kjelloh <kjell-olov.hogdahl@itfied.se>
2021-12-04 18:41:58 +0100

    Detects winning board in test

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -5,6 +5,7 @@
 #include <iterator>
 #include <array>
 #include <type_traits>
+#include <numeric>
 
 template<int N>
 struct Env {
@@ -12,10 +13,12 @@ struct Env {
     std::vector<int> draws{};
     using Row = std::array<int,N>;
     using Board = std::array<Row,N>;
-    int const BOARD_SIZE = N;
+    static int const BOARD_SIZE = N;
 
     std::vector<Board> boards{};
 };
+
+using Environment = Env<5>;
 
 namespace tokenize {
     auto split_int(auto const& line,char delim) {
@@ -43,7 +46,7 @@ namespace tokenize {
 extern char const* pTest;
 extern char const* pData; 
 
-auto env = Env<5>{pTest};
+Environment env{pTest};
 
 void read_draws(std::istringstream& in) {
     std::string line{};
@@ -101,8 +104,84 @@ void read_input() {
     print_draws();
     print_boards();
 }
-void create_boards() {}
-void play() {}
+void create_boards() {
+    /* Done in read_input...*/
+}
+
+void print_board_state(auto& board,auto& bm) {
+    std::cout << "\nBOARD STATE";
+    for (int i = 0; i < env.BOARD_SIZE; i++) {
+        std::cout << "\n";
+        std::copy(std::begin(board[i]),std::end(board[i]),std::ostream_iterator<int>(std::cout," "));
+        std::cout << "\t\t";
+        std::transform(std::begin(bm[i]),std::end(bm[i]),std::ostream_iterator<std::string>(std::cout," "),[](auto b) {
+             return (b?" X ":" - ");
+        });
+    }
+    std::cout << "\n";
+}
+
+void print_board_states(auto& bms) {
+    std::cout << "\nBOARD STATES";
+    for (int i = 0; i<bms.size();i++) {        
+        print_board_state(env.boards[i],bms[i]);
+    }
+}
+
+void mark_board(auto& board,auto& bm, auto number) {
+    for (int i = 0; i < env.BOARD_SIZE; i++) {
+        for (int j = 0; j < env.BOARD_SIZE; j++) {
+            if (board[i][j] == number) bm[i][j] = true;
+        }
+    }
+}
+
+void mark_boards(auto& bms,auto number) {
+    std::cout << "\nCALLING NUMBER " << number;
+    for (int i = 0; i<bms.size();i++) {
+        mark_board(env.boards[i],bms[i],number);
+        print_board_state(env.boards[i],bms[i]);
+    }
+}
+
+bool is_winning_board(auto& bm) {
+    bool result{};
+    if (std::any_of(std::begin(bm),std::end(bm),[](auto& bm_row){
+        return std::all_of(std::begin(bm_row),std::end(bm_row),[](auto& b) {
+            return b;
+        });
+    })) result = true;
+    else {
+        auto acc = std::accumulate(std::begin(bm),std::end(bm),bm[0],[](auto& acc,auto& bm_row) {
+            for (int i = 0; i<acc.size(); i++) acc[i]&=bm_row[i];
+            // print column flags
+            std::cout << "\nacc:";
+            std::transform(std::begin(acc),std::end(acc),std::ostream_iterator<std::string>(std::cout," "),[](auto& b) {
+                return (b?"X":"-");
+            });
+            return acc;
+        });
+        if (std::any_of(std::begin(acc),std::end(acc),[](auto b) {
+            return b;
+        })) result = true;
+    }
+    if (result) std::cout << "\nWinning Board!";
+    return result;
+}
+
+void play() {
+    using Mark = std::array<std::array<bool,Environment::BOARD_SIZE>,Environment::BOARD_SIZE>;
+    using BoardsMark = std::vector<Mark>;
+    BoardsMark bms{env.boards.size(),Mark{}};
+    for (auto const& number : env.draws) {
+        mark_boards(bms,number);
+        if (std::any_of(std::begin(bms),std::end(bms),[](auto& bm) {
+            return is_winning_board(bm);
+        })) break;
+        
+    }
+    print_board_states(bms);
+}
 auto calculate_answer() {return 0;}
 void print_answer(auto answer) {std::cout << "\nanswer=" << answer;}
 
@@ -125,7 +204,7 @@ int main(int argc, char *argv[]) {
     part1::solve();
 }
 
-char const* pTest = R"(7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+char const* pTest = R"(19,7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
 22 13 17 11  0
  8  2 23  4 24
