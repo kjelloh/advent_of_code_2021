cc447afeacb5d58394519901be4039092137000b
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2021-12-05 21:40:26 +0100

    day 5 added gcc-11 code to mimic Python string split in C++ pattern matching

--- day5/day5/day5/main.cpp
+++ day5/day5/day5/main.cpp
@@ -4,12 +4,60 @@
 //
 //  Created by Kjell-Olov Högdal on 2021-12-05.
 //
+#include <utility>
+#include <string>
+#include <algorithm>
+
+namespace python_inspired {
+    /*
+        Consider https://github.com/jonathanpaulson/AdventOfCode/blob/master/2021/5.py 
+        Inspiration for making C++ code that is not so talkative...
+
+import sys
+from collections import defaultdict
+
+infile = sys.argv[1] if len(sys.argv)>1 else '5.in'
+
+G1 = defaultdict(int)
+G2 = defaultdict(int)
+for line in open(infile):
+    start,end = line.split('->')
+    x1,y1 = start.split(',')
+    x2,y2 = end.split(',')
+    x1 = int(x1.strip())
+    y1 = int(y1.strip())
+    x2 = int(x2.strip())
+    y2 = int(y2.strip())
+
+    dx = x2-x1
+    dy = y2-y1
+
+    for i in range(1+max(abs(dx),abs(dy))):
+        x = x1+(1 if dx>0 else (-1 if dx<0 else 0))*i
+        y = y1+(1 if dy>0 else (-1 if dy<0 else 0))*i
+        if dx==0 or dy==0:
+            G1[(x,y)] += 1
+        G2[(x,y)] += 1
+
+print(len([k for k in G1 if G1[k]>1]))
+print(len([k for k in G2 if G2[k]>1]))            
+    */
+
+   // start,end = line.split('->')
+   template <typename T>
+   struct Line {
+       T line{};
+       std::pair<T,T> split(auto delim) const {
+           return {};
+        }
+   };
+   Line<std::string> line{"0,9 -> 5,9"};
+   auto [start,end] = line.split("->");
+}
 
 #include <iostream>
 #include <vector>
-#include <string>
 #include <sstream>
-#include <utility>
 #include <clocale>
 #include <map>
 #include <numeric>
