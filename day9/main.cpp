e209ecc6b64c60a35f4a0155685f3896fa62f251
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2021-12-08 22:36:41 +0100

    day9 prepared in Visual Studio 2022 (C++)

---
+++ day9/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <algorithm>
+
+extern char const* pTest;
+extern char const* pData;
+
+using Result = size_t;
+using Answers = std::vector<std::pair<std::string, Result>>;
+
+namespace part1 {
+    Result solve_for(char const* pData) {
+        return {};
+    }
+}
+
+namespace part2 {
+    Result solve_for(char const* pData) {
+        return {};
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    Answers answers{};
+    answers.push_back({ "Part 1 Test",part1::solve_for(pTest) });
+    answers.push_back({ "Part 1     ",part1::solve_for(pData) });
+    answers.push_back({ "Part 2 Test",part2::solve_for(pTest) });
+    answers.push_back({ "Part 2     ",part2::solve_for(pData) });
+    for (auto const& answer : answers) {
+        std::cout << "\nanswer[" << answer.first << "] " << answer.second;
+    }
+    std::cout << "\nPress <enter>...";
+    std::cin.get();
+    std::cout << "\n";
+    return 0;
+}
+
+char const* pTest = R"()";
+char const* pData = R"()";
