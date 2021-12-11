53897ea2fbb1e196cfe7872c3d474566b784fc7c
kjelloh <kjell-olov.hogdahl@itfied.se>
2021-12-11 17:42:33 +0100

    day 11 part 1&2 cleaned ouput to list all answers clearly (VSCode, g++-8, Debian 10)

--- day11/main.cpp
+++ day11/main.cpp
@@ -5,6 +5,11 @@
 #include <sstream>
 #include <algorithm>
 
+char const* pTest0 = R"(11111
+19991
+19191
+19991
+11111)";
 extern char const* pTest;
 extern char const* pData;
 
@@ -23,19 +28,154 @@ In parse(auto& in) {
 }
 
 namespace part1 {
-  Result solve_for(char const* pData) {
+  void flash(auto& grid, int r,int c) {
+    std::cout << "\nflash:{" << r << "," << c << "}=> ++ " ;
+    grid[r][c] = ' '; // mark as "flashed"
+    for (int dr : {-1,0,1}) {
+      for (int dc : {-1,0,1}) {
+        if (r+dr >= 0 and r+dr < grid.size() and c+dc>=0 and c+dc < grid[0].size() and grid[r+dr][c+dc]!=' ') {
+          ++grid[r+dr][c+dc];
+          std::cout << "{" << r+dr << "," << c+dc << "}";
+        }
+      }
+    }
+  } 
+  Result solve_for(char const* pData,int const LOOP_COUNT) {
       Result result{};
       std::stringstream in{ pData };
-      auto in_data = parse(in);
+      auto grid = parse(in);
+      int flash_count{0};
+      int step{0};
+      while (true) {
+        {
+          // increment
+          for (auto& row : grid) {
+            for (auto& digit : row) {
+              ++digit;
+            }
+          }
+        }
+        {
+          while (true) {
+            int cashed_flash_count{flash_count};
+            // find all == 9+1 and "flash"
+            for (int r=0;r<grid.size();r++) {
+              for (int c=0; c<grid[0].size();c++) {
+                if (grid[r][c] > '9') {
+                  // energy > '9' "to flash" and '0'-1 as "already flashed"
+                  flash(grid,r,c);
+                  ++flash_count;                  
+                }
+              }
+            }
+            if (flash_count==cashed_flash_count) break;      
+          }
+          std::cout << "\n";
+          for (auto& row : grid) {
+            std::cout << "\n";
+            for (auto& digit : row) {
+              std::cout << " " << digit;
+            }
+          }
+          {
+            // set energy of flashed to 0
+            for (auto& row : grid) {
+              for (auto& digit : row) {
+                digit = (digit==' ')?'0':digit;
+              }
+            }
+          }
+          std::cout << "\n";
+          for (auto& row : grid) {
+            std::cout << "\n";
+            for (auto& digit : row) {
+              std::cout << " " << digit;
+            }
+          }
+        }
+        ++step;
+        std::cout << "\nafter step: " << step << " flash_count: " << flash_count;
+        if (step<LOOP_COUNT) continue;
+        result = flash_count;
+        break;
+      }
       return result;
   }
 }
 
 namespace part2 {
+  void flash(auto& grid, int r,int c) {
+    std::cout << "\nflash:{" << r << "," << c << "}=> ++ " ;
+    grid[r][c] = ' '; // mark as "flashed"
+    for (int dr : {-1,0,1}) {
+      for (int dc : {-1,0,1}) {
+        if (r+dr >= 0 and r+dr < grid.size() and c+dc>=0 and c+dc < grid[0].size() and grid[r+dr][c+dc]!=' ') {
+          ++grid[r+dr][c+dc];
+          std::cout << "{" << r+dr << "," << c+dc << "}";
+        }
+      }
+    }
+  } 
   Result solve_for(char const* pData) {
       Result result{};
       std::stringstream in{ pData };
-      auto in_data = parse(in);
+      auto grid = parse(in);
+      int flash_count{0};
+      int step{0};
+      while (true) {
+        int cashed_flash_count{flash_count};
+        {
+          // increment
+          for (auto& row : grid) {
+            for (auto& digit : row) {
+              ++digit;
+            }
+          }
+        }
+        {
+          while (true) {
+            int cashed_flash_count{flash_count};
+            // find all == 9+1 and "flash"
+            for (int r=0;r<grid.size();r++) {
+              for (int c=0; c<grid[0].size();c++) {
+                if (grid[r][c] > '9') {
+                  // energy > '9' "to flash" and '0'-1 as "already flashed"
+                  flash(grid,r,c);
+                  ++flash_count;                  
+                }
+              }
+            }
+            if (flash_count==cashed_flash_count) break;      
+          }
+          std::cout << "\n";
+          for (auto& row : grid) {
+            std::cout << "\n";
+            for (auto& digit : row) {
+              std::cout << " " << digit;
+            }
+          }
+          {
+            // set energy of flashed to 0
+            for (auto& row : grid) {
+              for (auto& digit : row) {
+                digit = (digit==' ')?'0':digit;
+              }
+            }
+          }
+          std::cout << "\n";
+          for (auto& row : grid) {
+            std::cout << "\n";
+            for (auto& digit : row) {
+              std::cout << " " << digit;
+            }
+          }
+        }
+        ++step;
+        std::cout << "\nafter step: " << step << " flash_count: " << flash_count;
+        if ((flash_count - cashed_flash_count)<100) continue;
+        result = step;
+        break;
+      }
       return result;
   }
 }
@@ -43,18 +183,38 @@ namespace part2 {
 int main(int argc, char *argv[])
 {
   Answers answers{};
-  answers.push_back({"Part 1 Test",part1::solve_for(pTest)});
-  // answers.push_back({"Part 1     ",part1::solve_for(pData)});
-  // answers.push_back({"Part 2 Test",part2::solve_for(pTest)});
-  // answers.push_back({"Part 2     ",part2::solve_for(pData)});
+  answers.push_back({"Part 1 Test small 2 steps",part1::solve_for(pTest0,2)});
+  answers.push_back({"Part 1 Test full 10 steps",part1::solve_for(pTest,10)});
+  answers.push_back({"Part 1 Test full 100 steps",part1::solve_for(pTest,100)});
+  answers.push_back({"Part 1     ",part1::solve_for(pData,100)});
+  answers.push_back({"Part 2 Test",part2::solve_for(pTest)});
+  answers.push_back({"Part 2     ",part2::solve_for(pData)});
   for (auto const& answer : answers) {
     std::cout << "\nanswer[" << answer.first << "] " << answer.second;
   }
-  std::cout << "\nPress <enter>...";
-  std::cin.get();
+  // std::cout << "\nPress <enter>...";
+  // std::cin.get();
   std::cout << "\n";
   return 0;
 }
 
-char const* pTest = R"()";
-char const* pData = R"()";
+char const* pTest = R"(5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526)";
+char const* pData = R"(8258741254
+3335286211
+8468661311
+6164578353
+2138414553
+1785385447
+3441133751
+3586862837
+7568272878
+6833643144)";
