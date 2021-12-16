f507df25f832b880120fd93d2c71c42b4a155566
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2021-12-16 06:46:34 +0100

    day16 parse to binary string OK (XCode 13.2 Beta 2, C++20, macOS)

---
+++ day16/main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <algorithm>
+#include <bitset>
+
+extern char const* pTest;
+extern char const* pData;
+
+using Result = size_t;
+using Answers = std::vector<std::pair<std::string,Result>>;
+
+using BitString = std::string;
+using Model = BitString;
+
+BitString to_bit_string(char hex_digit) {
+  BitString result;
+  std::bitset<4> bit_set{};
+  switch (hex_digit) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      bit_set = std::bitset<4>{static_cast<size_t>(hex_digit-'0')};
+      break;
+    case 'A':
+    case 'B':
+    case 'C':
+    case 'D':
+    case 'E':
+    case 'F':
+      bit_set = std::bitset<4>{static_cast<size_t>(hex_digit-'A' + 0x0A)};
+      break;
+  }
+  result = bit_set.to_string();
+  return result;
+}
+
+Model parse(auto& in) {
+  Model result{};
+  std::string line{};
+  while (in >> line) {
+    std::cout << "\nin HEX[" << line.size() << "]:" << line;
+  }
+  for (auto const& hex_digit : line) {
+    result += to_bit_string(hex_digit);
+  }
+  return result;
+}
+
+namespace part1 {
+  Result solve_for(char const* pData) {
+    Result result{};
+    std::stringstream in{ pData };
+    auto data_model = parse(in);
+    std::cout << "\nin binary:[" << data_model.size() << "]" << data_model;
+    return result;
+  }
+}
+
+namespace part2 {
+  Result solve_for(char const* pData) {
+      Result result{};
+      std::stringstream in{ pData };
+      auto data_model = parse(in);
+      return result;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  Answers answers{};
+  // answers.push_back({"Part 1 Test",part1::solve_for(pTest)});
+  answers.push_back({"Part 1     ",part1::solve_for(pData)});
+  // answers.push_back({"Part 2 Test",part2::solve_for(pTest)});
+  // answers.push_back({"Part 2     ",part2::solve_for(pData)});
+  for (auto const& answer : answers) {
+    std::cout << "\nanswer[" << answer.first << "] " << answer.second;
+  }
+  // std::cout << "\nPress <enter>...";
+  // std::cin.get();
+  std::cout << "\n";
+  return 0;
+}
+
+char const* pTest = R"()";
+char const* pData = R"(00569F4A0488043262D30B333FCE6938EC5E5228F2C78A017CD78C269921249F2C69256C559CC01083BA00A4C5730FF12A56B1C49A480283C0055A532CF2996197653005FC01093BC4CE6F5AE49E27A7532200AB25A653800A8CAE5DE572EC40080CD26CA01CAD578803CBB004E67C573F000958CAF5FC6D59BC8803D1967E0953C68401034A24CB3ACD934E311004C5A00A4AB9CAE99E52648401F5CC4E91B6C76801F59DA63C1F3B4C78298014F91BCA1BAA9CBA99006093BFF916802923D8CC7A7A09CA010CD62DF8C2439332A58BA1E495A5B8FA846C00814A511A0B9004C52F9EF41EC0128BF306E4021FD005CD23E8D7F393F48FA35FCE4F53191920096674F66D1215C98C49850803A600D4468790748010F8430A60E1002150B20C4273005F8012D95EC09E2A4E4AF7041004A7F2FB3FCDFA93E4578C0099C52201166C01600042E1444F8FA00087C178AF15E179802F377EC695C6B7213F005267E3D33F189ABD2B46B30042655F0035300042A0F47B87A200EC1E84306C801819B45917F9B29700AA66BDC7656A0C49DB7CAEF726C9CEC71EC5F8BB2F2F37C9C743A600A442B004A7D2279125B73127009218C97A73C4D1E6EF64A9EFDE5AF4241F3FA94278E0D9005A32D9C0DD002AB2B7C69B23CCF5B6C280094CE12CDD4D0803CF9F96D1F4012929DA895290FF6F5E2A9009F33D796063803551006E3941A8340008743B8D90ACC015C00DDC0010B873052320002130563A4359CF968000B10258024C8DF2783F9AD6356FB6280312EBB394AC6FE9014AF2F8C381008CB600880021B0AA28463100762FC1983122D2A005CBD11A4F7B9DADFD110805B2E012B1F4249129DA184768912D90B2013A4001098391661E8803D05612C731007216C768566007280126005101656E0062013D64049F10111E6006100E90E004100C1620048009900020E0006DA0015C000418000AF80015B3D938)";
