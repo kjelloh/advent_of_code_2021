438b898449315bf4ec597e54fb99d131cec5911b
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2021-12-17 22:28:22 +0100

    day17 part 2 - wrong answer for some reason...

--- day17/main.cpp
+++ day17/main.cpp
@@ -13,15 +13,16 @@ using Result = size_t;
 using Answers = std::vector<std::pair<std::string,Result>>;
 using Coord = int;
 using CoordRange = std::pair<Coord, Coord>;
-struct Position {
-  std::pair<Coord, Coord> coord;
-  Coord row() { return coord.second; }
-  Coord col() { return coord.first; }
-};
 struct Area {
   CoordRange x_range, y_range;
 };
-using Model = Area;
+using Shot = std::pair<int, int>;
+using Shots = std::vector<Shot>;
+struct TargetPractice {
+  Area target;
+  Shots shots;
+};
+using Model = TargetPractice;
 
 std::pair<std::string, std::string> split(std::string const& token, std::string const& delim) {
   auto pos = token.find(delim);
@@ -36,7 +37,6 @@ std::pair<std::string, std::string> split(std::string const& token, char delim) 
 CoordRange coord_range(std::string const& sc1, std::string const& sc2) {
   auto c1 = std::stoi(sc1);
   auto c2 = std::stoi(sc2);
-  // Range so that the coord closer to 0 is first
   if (c1 < c2) {
     return { c1,c2 };
   }
@@ -61,17 +61,16 @@ Model parse(auto& in) {
     auto [sy1,sy2] = split(tail_range, "..");
     auto x_range = coord_range(sx1,sx2);
     auto y_range = coord_range(sy1, sy2);
-    return {x_range,y_range};
+    TargetPractice target_practice{ {x_range,y_range},Shots{} };
+    while (in >> line) {
+      if (line.size() > 0) {
+        auto [dx_s, dy_s] = split(line, ',');
+        std::cout << "\nshot " << dx_s << " " << dy_s;
+        target_practice.shots.push_back({ std::stoi(dx_s),std::stoi(dy_s) });
+      }
+    }
+    return target_practice;
 }
-
-//int x(auto v0, auto n) {
-//  if (n > v0) return x(v0,v0);
-//  else return (n * v0 + n * (1 - n) / 2);
-//}
-//
-//int y(auto v0,auto n) {
-//  return (n*v0+n*(1-n)/2);
-//}
 
 using MaxY = int;
 using OptionalMaxY = std::optional<MaxY>;
@@ -84,7 +83,10 @@ bool may_hit_target(Area const& target,int x, int y, int dx, int dy) {
   // Note: if dy>0 it decrements towards dy<0 so we cant know for sure it may not continue up or later come down to y_range
   return true;
 }
+int global_shot_count{ 0 };
 OptionalMaxY max_y_if_target_hit(Area const& target,int dx,int dy) {
+  std::cout << "\nmax_y_if_target_hit dx:" << dx << " dy:" << dy;
+  global_shot_count++;
   MaxY result{0};
   bool found_one = false;
   int x = dx;
@@ -93,7 +95,7 @@ OptionalMaxY max_y_if_target_hit(Area const& target,int dx,int dy) {
     //std::cout << "\ntest {x:" << x << ",y:" << y << "}";
     result = std::max(result,y);
     if (in_range(x,target.x_range) and in_range(y,target.y_range)) {
-      //std::cout << " ==> in target! max_y=" << result;
+      std::cout << " ==> in target! max_y=" << result;
       found_one = true;
       break;
     }
@@ -109,7 +111,8 @@ namespace part1 {
   Result solve_for(char const* pData) {
       Result result{};
       std::stringstream in{ pData };
-      auto target_area = parse(in);
+      auto target_practice = parse(in);
+      auto target_area = target_practice.target;
       std::cout << "\ntarget area:"
         << " x_range [" << target_area.x_range.first << ".." << target_area.x_range.second << "]"
         << " y_range [" << target_area.y_range.first << ".." << target_area.y_range.second << "]";
@@ -137,7 +140,17 @@ namespace part2 {
   Result solve_for(char const* pData) {
       Result result{};
       std::stringstream in{ pData };
-      auto data_model = parse(in);
+      auto target_practice = parse(in);
+      std::cout << "\nshot count:" << target_practice.shots.size();
+      int hit_count{ 0 };
+      global_shot_count = 0;
+      for (auto const& shot : target_practice.shots) {
+        if (auto optional_max_y = max_y_if_target_hit(target_practice.target, shot.first, shot.second)) {
+          ++hit_count;
+          std::cout << "\n\thit count:" << hit_count << " global shot count:" << global_shot_count;
+        }
+      }
+      result = hit_count;
       return result;
   }
 }
@@ -145,10 +158,9 @@ namespace part2 {
 int main(int argc, char *argv[])
 {
   Answers answers{};
-  answers.push_back({"Part 1 Test",part1::solve_for(pTest)});
-  answers.push_back({"Part 1     ",part1::solve_for(pData)});
-  // answers.push_back({"Part 2 Test",part2::solve_for(pTest)});
-  // answers.push_back({"Part 2     ",part2::solve_for(pData)});
+  //answers.push_back({"Part 1 Test",part1::solve_for(pTest)});
+  // answers.push_back({"Part 1     ",part1::solve_for(pData)});
+  answers.push_back({"Part 2     ",part2::solve_for(pData)});
   for (auto const& answer : answers) {
     std::cout << "\nanswer[" << answer.first << "] " << answer.second;
   }
@@ -159,4 +171,17 @@ int main(int argc, char *argv[])
 }
 
 char const* pTest = R"(target area: x=20..30, y=-10..-5)";
-char const* pData = R"(target area: x=137..171, y=-98..-73)";
+char const* pData = R"(target area: x=137..171, y=-98..-73
+23,-10  25,-9   27,-5   29,-6   22,-6   21,-7   9,0     27,-7   24,-5
+25,-7   26,-6   25,-5   6,8     11,-2   20,-5   29,-10  6,3     28,-7
+8,0     30,-6   29,-8   20,-10  6,7     6,4     6,1     14,-4   21,-6
+26,-10  7,-1    7,7     8,-1    21,-9   6,2     20,-7   30,-10  14,-3
+20,-8   13,-2   7,3     28,-8   29,-9   15,-3   22,-5   26,-8   25,-8
+25,-6   15,-4   9,-2    15,-2   12,-2   28,-9   12,-3   24,-6   23,-7
+25,-10  7,8     11,-3   26,-7   7,1     23,-9   6,0     22,-10  27,-6
+8,1     22,-8   13,-4   7,6     28,-6   11,-4   12,-4   26,-9   7,4
+24,-10  23,-8   30,-8   7,0     9,-1    10,-1   26,-5   22,-9   6,5
+7,5     23,-6   28,-10  10,-2   11,-1   20,-9   14,-2   29,-7   13,-3
+23,-5   24,-8   27,-9   30,-7   28,-5   21,-10  7,9     6,6     21,-5
+27,-10  7,2     30,-9   21,-8   22,-7   24,-9   20,-6   6,9     29,-5
+8,-2    27,-8   30,-5   24,-7)";
