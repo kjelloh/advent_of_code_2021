1289782a3d58c2aa179352e249abcb1cda624392
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2021-12-22 10:51:48 +0100

    day22 part 1 ok - cleaned up (VSCode, g++-11, c++20, macOS)

--- day22/main.cpp
+++ day22/main.cpp
@@ -7,6 +7,12 @@
 #include <array>
 #include <set>
 
+const char* pSnippet = R"(on x=10..12,y=10..12,z=10..12
+on x=11..13,y=11..13,z=11..13
+off x=9..11,y=9..11,z=9..11
+on x=10..10,y=10..10,z=10..10)";
+const char* pSnippet0 = R"(on x=-20..26,y=-36..17,z=-47..7)";
+
 extern char const* pTest;
 extern char const* pData;
 
@@ -39,8 +45,8 @@ public:
     os << " {" << std::to_string(m_xr.first) << "," << std::to_string(m_yr.first) << "," << std::to_string(m_zr.first) << "}";
     os << " .. ";
     os << " {" << std::to_string(m_xr.second) << "," << std::to_string(m_yr.second) << "," << std::to_string(m_zr.second) << "}";
-    if (m_on) os << " ON";
-    else os << "off";
+    if (m_on) os << " ON!";
+    else os << " off";
     return os.str();
   }
   void for_each_50(auto f) const {
@@ -70,7 +76,28 @@ std::pair<std::string, std::string> split(std::string const& token,std::string d
 Model parse(auto& in) {
     Model result{};
     std::string line{};
-    while (in >> line) {
+    while (std::getline(in, line)) {
+      auto [left, right] = split(line, " ");
+      // std::cout << "left \"" << left << "\" right \"" << right << "\"";
+      bool on = left == "on";
+      auto [xs, right_x] = split(right, ",");
+      auto [ys, right_y] = split(right_x, ",");
+      auto [zs, right_z] = split(right_y, ",");
+      // std::cout << "\nx:" << xs << " y:" << ys << " z:" << zs;
+      auto [xs_left, xs_right] = split(xs, "=");
+      auto [ys_left, ys_right] = split(ys, "=");
+      auto [zs_left, zs_right] = split(zs, "=");
+      // std::cout << "x:" << xs_right << " y:" << ys_right << " z:" << zs_right;
+      auto [xs1, xs2] = split(xs_right, "..");
+      auto [ys1, ys2] = split(ys_right, "..");
+      auto [zs1, zs2] = split(zs_right, "..");
+      // std::cout << "\n" << "x:" << xs1 << ".." << xs2 << " y:" << ys1 << ".." << ys2 << " z:" << zs1 << ".." << zs2;
+      Cuboid cuboid{ on,{xs1,xs2},{ys1,ys2},{zs1,zs2} };
+      result.push_back(cuboid);
+    }
+    std::cout << "\ncuboid count " << result.size();
+    for (auto const& cuboid : result) {
+      std::cout << "\n" << cuboid.to_string();
     }
     return result;
 }
@@ -98,69 +125,13 @@ private:
 namespace part1 {
   Result solve_for(char const* pData) {
       Result result{};
-      {
-        Model puzzle_model{};
-        const char* pSnippet = R"(on x=10..12,y=10..12,z=10..12
-on x=11..13,y=11..13,z=11..13
-off x=9..11,y=9..11,z=9..11
-on x=10..10,y=10..10,z=10..10)";
-        const char* pSnippet0 = R"(on x=-20..26,y=-36..17,z=-47..7)";
-        const char* pSnippet2 = R"(on x=-20..26,y=-36..17,z=-47..7
-on x=-20..33,y=-21..23,z=-26..28
-on x=-22..28,y=-29..23,z=-38..16
-on x=-46..7,y=-6..46,z=-50..-1
-on x=-49..1,y=-3..46,z=-24..28
-on x=2..47,y=-22..22,z=-23..27
-on x=-27..23,y=-28..26,z=-21..29
-on x=-39..5,y=-6..47,z=-3..44
-on x=-30..21,y=-8..43,z=-13..34
-on x=-22..26,y=-27..20,z=-29..19
-off x=-48..-32,y=26..41,z=-47..-37
-on x=-12..35,y=6..50,z=-50..-2
-off x=-48..-32,y=-32..-16,z=-15..-5
-on x=-18..26,y=-33..15,z=-7..46
-off x=-40..-22,y=-38..-28,z=23..41
-on x=-16..35,y=-41..10,z=-47..6
-off x=-32..-23,y=11..30,z=-14..3
-on x=-49..-5,y=-3..45,z=-29..18
-off x=18..30,y=-20..-8,z=-3..13
-on x=-41..9,y=-7..43,z=-33..15
-on x=-54112..-39298,y=-85059..-49293,z=-27449..7877
-on x=967..23432,y=45373..81175,z=27513..53682)";
-        std::stringstream in{ pData };
-        std::string line{};
-        while (std::getline(in, line)) {
-          auto [left, right] = split(line, " ");
-          // std::cout << "left \"" << left << "\" right \"" << right << "\"";
-          bool on = left == "on";
-          auto [xs, right_x] = split(right, ",");
-          auto [ys, right_y] = split(right_x, ",");
-          auto [zs, right_z] = split(right_y, ",");
-          // std::cout << "\nx:" << xs << " y:" << ys << " z:" << zs;
-          auto [xs_left, xs_right] = split(xs, "=");
-          auto [ys_left, ys_right] = split(ys, "=");
-          auto [zs_left, zs_right] = split(zs, "=");
-          // std::cout << "x:" << xs_right << " y:" << ys_right << " z:" << zs_right;
-          auto [xs1, xs2] = split(xs_right, "..");
-          auto [ys1, ys2] = split(ys_right, "..");
-          auto [zs1, zs2] = split(zs_right, "..");
-          // std::cout << "\n" << "x:" << xs1 << ".." << xs2 << " y:" << ys1 << ".." << ys2 << " z:" << zs1 << ".." << zs2;
-          Cuboid cuboid{ on,{xs1,xs2},{ys1,ys2},{zs1,zs2} };
-          puzzle_model.push_back(cuboid);
-        }
-        std::cout << "\ncuboid count " << puzzle_model.size();
-        for (auto const& cuboid : puzzle_model) {
-          std::cout << "\n" << cuboid.to_string();
-        }
-        Reactor reactor{};
-        for (auto const& cuboid : puzzle_model) {
-          reactor.set_cubes(cuboid);
-        }
-        result = reactor.cubes_on_count();
-
-      }
       std::stringstream in{ pData };
-      auto data_model = parse(in);
+      auto puzzle_model = parse(in);
+      Reactor reactor{};
+      for (auto const& cuboid : puzzle_model) {
+        reactor.set_cubes(cuboid);
+      }
+      result = reactor.cubes_on_count();
       return result;
   }
 }
@@ -177,7 +148,7 @@ namespace part2 {
 int main(int argc, char *argv[])
 {
   Answers answers{};
-  // answers.push_back({"Part 1 Test",part1::solve_for(pTest)});
+  //answers.push_back({"Part 1 Test",part1::solve_for(pTest)});
   answers.push_back({"Part 1     ",part1::solve_for(pData)});
   // answers.push_back({"Part 2 Test",part2::solve_for(pTest)});
   // answers.push_back({"Part 2     ",part2::solve_for(pData)});
