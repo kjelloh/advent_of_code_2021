3c5c4f849f434b062ce851135874dae008ce1e40
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2021-12-22 23:17:44 +0100

    day22 part 2 as tought by Neal Wu and Jonathan Paulson (but wrong answer so far...)

--- day22/main.cpp
+++ day22/main.cpp
@@ -6,14 +6,61 @@
 #include <algorithm>
 #include <array>
 #include <set>
+#include <iterator>
 
-const char* pSnippet = R"(on x=10..12,y=10..12,z=10..12
+const char* pSnippet1 = R"(on x=10..12,y=10..12,z=10..12
 on x=11..13,y=11..13,z=11..13
 off x=9..11,y=9..11,z=9..11
 on x=10..10,y=10..10,z=10..10)";
 const char* pSnippet0 = R"(on x=-20..26,y=-36..17,z=-47..7)";
+const char* pSnippet2 = R"(on x=-20..26,y=-36..17,z=-47..7
+off x=-20..26,y=-36..17,z=-47..7)";
+const char* pSnippet3 = R"(on x=-20..26,y=-36..17,z=-47..7
+on x=-20..33,y=-21..23,z=-26..28
+on x=-22..28,y=-29..23,z=-38..16
+on x=-46..7,y=-6..46,z=-50..-1
+on x=-49..1,y=-3..46,z=-24..28
+on x=2..47,y=-22..22,z=-23..27
+on x=-27..23,y=-28..26,z=-21..29
+on x=-39..5,y=-6..47,z=-3..44
+on x=-30..21,y=-8..43,z=-13..34
+on x=-22..26,y=-27..20,z=-29..19
+off x=-48..-32,y=26..41,z=-47..-37
+on x=-12..35,y=6..50,z=-50..-2
+off x=-48..-32,y=-32..-16,z=-15..-5
+on x=-18..26,y=-33..15,z=-7..46
+off x=-40..-22,y=-38..-28,z=23..41
+on x=-16..35,y=-41..10,z=-47..6
+off x=-32..-23,y=11..30,z=-14..3
+on x=-49..-5,y=-3..45,z=-29..18
+off x=18..30,y=-20..-8,z=-3..13
+on x=-41..9,y=-7..43,z=-33..15)";
 
-extern char const* pTest;
+/*
+<cuboid> {-20,-36,-47} ..  {26,17,7} ON!
+<cuboid> {-20,-21,-26} ..  {33,23,28} ON!
+<cuboid> {-22,-29,-38} ..  {28,23,16} ON!
+<cuboid> {-46,-6,-50} ..  {7,46,-1} ON!
+<cuboid> {-49,-3,-24} ..  {1,46,28} ON!
+<cuboid> {2,-22,-23} ..  {47,22,27} ON!
+<cuboid> {-27,-28,-21} ..  {23,26,29} ON!
+<cuboid> {-39,-6,-3} ..  {5,47,44} ON!
+<cuboid> {-30,-8,-13} ..  {21,43,34} ON!
+<cuboid> {-22,-27,-29} ..  {26,20,19} ON!
+<cuboid> {-48,26,-47} ..  {-32,41,-37} off
+<cuboid> {-12,6,-50} ..  {35,50,-2} ON!
+<cuboid> {-48,-32,-15} ..  {-32,-16,-5} off
+<cuboid> {-18,-33,-7} ..  {26,15,46} ON!
+<cuboid> {-40,-38,23} ..  {-22,-28,41} off
+<cuboid> {-16,-41,-47} ..  {35,10,6} ON!
+<cuboid> {-32,11,-14} ..  {-23,30,3} off
+<cuboid> {-49,-3,-29} ..  {-5,45,18} ON!
+<cuboid> {18,-20,-3} ..  {30,-8,13} off
+<cuboid> {-41,-7,-33} ..  {9,43,15} ON!
+*/
+
+extern char const* pTestp1;
+extern char const* pTestp2;
 extern char const* pData;
 
 using Result = size_t;
@@ -36,9 +83,9 @@ class Cuboid {
 public:
   Cuboid(bool on,CoordRange<std::string> xr, CoordRange<std::string> yr, CoordRange<std::string> zr)
     :  m_on{on}
-      ,m_xr{ std::stoi(xr.first),std::stoi(xr.second)}
-      ,m_yr{std::stoi(yr.first),std::stoi(yr.second)}
-      ,m_zr{std::stoi(zr.first),std::stoi(zr.second)} {}
+      ,m_xr{ coord_range(xr)}
+      ,m_yr{ coord_range(yr)}
+      ,m_zr{ coord_range(zr)} {}
   std::string to_string() const {
     std::ostringstream os{};
     os << "<cuboid>";
@@ -49,6 +96,10 @@ public:
     else os << " off";
     return os.str();
   }
+  bool on() const {return m_on;}
+  CoordRange<Coord> x_range() const {return m_xr;}
+  CoordRange<Coord> y_range() const {return m_yr;}
+  CoordRange<Coord> z_range() const {return m_zr;}
   void for_each_50(auto f) const {
     for (Coord x = std::max(-50,m_xr.first); x <= std::min(50,m_xr.second); x++) {
       for (Coord y = std::max(-50,m_yr.first); y <= std::min(50,m_yr.second); y++) {
@@ -60,6 +111,12 @@ public:
   }
 private:
   bool m_on;
+  CoordRange<Coord> coord_range(CoordRange<std::string> const& r) {
+    auto first = std::stoi(r.first);
+    auto second = std::stoi(r.second);
+    // return {std::min(first,second),std::max(first,second)};
+    return {first,second};
+  }
   CoordRange<Coord> m_xr;
   CoordRange<Coord> m_yr;
   CoordRange<Coord> m_zr;
@@ -140,7 +197,97 @@ namespace part2 {
   Result solve_for(char const* pData) {
       Result result{};
       std::stringstream in{ pData };
-      auto data_model = parse(in);
+      auto cuboids = parse(in);
+      // Kudos to Jonathan Paulsson for teaching me about this approach (https://youtu.be/7gW_h0RTDd8?t=2653)
+      // Kudos to Neal Wu for his beatiful C++ teaching of the same approach (https://youtu.be/YKpViLcTp64?t=882)
+
+      // Observation 1: All we need to process are the cuboid boundaries and the state of cubes within each cuboid.
+      // "Proof": All cube state switching happens at cuboid faces. Between such faces all cubes have the same state.
+
+      // Approach - compress down the huge 3D cube space to a matrix of all cuboid boundaries.
+      // let this grid keep track of the boolean state "on" of each volume defined by all the boundaries 
+      // of the applied cuboids.
+      std::vector<std::vector<std::vector<bool>>> on_off_grid{};
+      // Observation 2: if we apply the "on" and "off" for each space defined by each cuboid in this on/off grid
+      // we should get the final cube states represented by the grid.
+
+      // Problem 1: We need to map the huge 3D cube space to indexies in the on_off_grid matrix.
+      // Solution part 1: Create one vector for each dimension and put the cube boundaries into them.
+      std::vector<Coord> x_boundaries{},y_boundaries{},z_boundaries{};
+      for (auto const& cuboid : cuboids) {
+        // trap! Don't forget to add 1 to range.second to the the correct "volume" (include second face cubes to)
+        x_boundaries.push_back(cuboid.x_range().first);
+        x_boundaries.push_back(cuboid.x_range().second+1); // +1 to include end coordinate in [first..second[
+        y_boundaries.push_back(cuboid.y_range().first);
+        y_boundaries.push_back(cuboid.y_range().second+1); // +1 to include end coordinate in [first..second[
+        z_boundaries.push_back(cuboid.z_range().first);
+        z_boundaries.push_back(cuboid.z_range().second+1); // +1 to include end coordinate in [first..second[
+      }
+
+      // Surprise! Sort the boundaries vectors! (We will figure out below how to still get the mapping correct)
+      std::sort(x_boundaries.begin(),x_boundaries.end());
+      std::sort(y_boundaries.begin(),y_boundaries.end());
+      std::sort(z_boundaries.begin(),z_boundaries.end());
+
+      // Observation: the size of the boundary vectors now defines the dimensions of the on_off_grid
+      auto const boundaries_count = x_boundaries.size();
+      // We can now init the on_off_grid with correct dimensions and init state "off" for all "space"
+      on_off_grid = std::vector(boundaries_count,std::vector(boundaries_count,std::vector(boundaries_count,false)));
+      // Solution part 2: Imagine a function on_off_grid_index and use it to fill the on_off_grid
+      // as definde by each cuboid to apply
+      auto on_off_grid_index = [](auto const& boundaries,Coord c)  {
+        // Return the index in the boundaries vector of the provided 3d boundary c.
+        // This is the distance from the beginning of the sorted boundaries vector
+        // to the position of c in the vector
+        auto iter = std::lower_bound(boundaries.begin(),boundaries.end(),c);
+        // Note: We need to use lower_bound (and not find) as we added end+1 for the end-boundary so when
+        // called with a cuboid end boundary it will not be exactly found. the lower_bound will :)
+        return std::distance(boundaries.begin(),iter);
+      };
+      for (auto const& cuboid : cuboids) {
+        auto on_off_x_first = on_off_grid_index(x_boundaries,cuboid.x_range().first);
+        auto on_off_x_second = on_off_grid_index(x_boundaries,cuboid.x_range().second);
+        auto on_off_y_first = on_off_grid_index(y_boundaries,cuboid.y_range().first);
+        auto on_off_y_second = on_off_grid_index(y_boundaries,cuboid.y_range().second);
+        auto on_off_z_first = on_off_grid_index(z_boundaries,cuboid.z_range().first);
+        auto on_off_z_second = on_off_grid_index(z_boundaries,cuboid.z_range().second);
+        bool state = cuboid.on();
+        // Observation: The on_off_grid "space" is defined by the index in x,y,z for each boundary 
+        // of all the 3d space cuboids.
+        // So what we need to do now is to apply the on/off state switching to the on_off_grid
+        // as defined by the applied cuboid.
+        for (int on_off_x=on_off_x_first;on_off_x<on_off_x_second;on_off_x++) {
+          for (int on_off_y=on_off_y_first;on_off_y<on_off_y_second;on_off_y++) {
+            for (int on_off_z=on_off_z_first;on_off_z<on_off_z_second;on_off_z++) {
+              on_off_grid[on_off_x][on_off_y][on_off_z] = state; // For this cuboid, set this state
+              // Note that overlapping cuboids will overwrite prevous state
+              // in the overlapping region (just as they should)
+            }            
+          }
+        }
+      }
+      // Now Magic has happened! The on_off_grid now represents the on/off state of the 3d space.
+      // We "just" needs to map the on-regions back to actual 3d space dimensions to get the
+      // "volume" of all  3d space that is "on".
+      // Note: loop to size-1 to take into account that we use index+1 to get volume sides below
+      Result on_volume_count{0};
+      for (int on_off_x=0;on_off_x < boundaries_count-1;on_off_x++) {
+        for (int on_off_y=0;on_off_y < boundaries_count-1;on_off_y++) {
+          for (int on_off_z=0;on_off_z < boundaries_count-1;on_off_z++) {
+            if (on_off_grid[on_off_x][on_off_y][on_off_z]) {
+              // Note: the reason we added +1 when we added range.second to the boundaries vectors above,
+              // were to get this "volume" correct (second-first) includes "second face".
+              auto dx = (x_boundaries[on_off_x+1] - x_boundaries[on_off_x]);
+              auto dy = (y_boundaries[on_off_y+1] - y_boundaries[on_off_y]);
+              auto dz = (z_boundaries[on_off_z+1] - z_boundaries[on_off_z]);
+              on_volume_count += dx*dy*dz;
+            }
+          }
+        }
+      }
+      result = on_volume_count;
+      // 2758514936282235 expected to part 2 test
+      // 55034886912308   is 100 times to small...
       return result;
   }
 }
@@ -148,9 +295,10 @@ namespace part2 {
 int main(int argc, char *argv[])
 {
   Answers answers{};
-  //answers.push_back({"Part 1 Test",part1::solve_for(pTest)});
-  answers.push_back({"Part 1     ",part1::solve_for(pData)});
-  // answers.push_back({"Part 2 Test",part2::solve_for(pTest)});
+  //answers.push_back({"Part 1 Test",part1::solve_for(pTestp1)});
+  // answers.push_back({"Part 1     ",part1::solve_for(pData)});
+  // answers.push_back({"Part 2 Test",part2::solve_for(pSnippet2)});
+  answers.push_back({"Part 2 Test",part2::solve_for(pTestp2)});
   // answers.push_back({"Part 2     ",part2::solve_for(pData)});
   for (auto const& answer : answers) {
     std::cout << "\nanswer[" << answer.first << "] " << answer.second;
@@ -161,7 +309,7 @@ int main(int argc, char *argv[])
   return 0;
 }
 
-char const* pTest = R"(on x=-20..26,y=-36..17,z=-47..7
+char const* pTestp1 = R"(on x=-20..26,y=-36..17,z=-47..7
 on x=-20..33,y=-21..23,z=-26..28
 on x=-22..28,y=-29..23,z=-38..16
 on x=-46..7,y=-6..46,z=-50..-1
@@ -183,6 +331,66 @@ off x=18..30,y=-20..-8,z=-3..13
 on x=-41..9,y=-7..43,z=-33..15
 on x=-54112..-39298,y=-85059..-49293,z=-27449..7877
 on x=967..23432,y=45373..81175,z=27513..53682)";
+char const* pTestp2 = R"(on x=-5..47,y=-31..22,z=-19..33
+on x=-44..5,y=-27..21,z=-14..35
+on x=-49..-1,y=-11..42,z=-10..38
+on x=-20..34,y=-40..6,z=-44..1
+off x=26..39,y=40..50,z=-2..11
+on x=-41..5,y=-41..6,z=-36..8
+off x=-43..-33,y=-45..-28,z=7..25
+on x=-33..15,y=-32..19,z=-34..11
+off x=35..47,y=-46..-34,z=-11..5
+on x=-14..36,y=-6..44,z=-16..29
+on x=-57795..-6158,y=29564..72030,z=20435..90618
+on x=36731..105352,y=-21140..28532,z=16094..90401
+on x=30999..107136,y=-53464..15513,z=8553..71215
+on x=13528..83982,y=-99403..-27377,z=-24141..23996
+on x=-72682..-12347,y=18159..111354,z=7391..80950
+on x=-1060..80757,y=-65301..-20884,z=-103788..-16709
+on x=-83015..-9461,y=-72160..-8347,z=-81239..-26856
+on x=-52752..22273,y=-49450..9096,z=54442..119054
+on x=-29982..40483,y=-108474..-28371,z=-24328..38471
+on x=-4958..62750,y=40422..118853,z=-7672..65583
+on x=55694..108686,y=-43367..46958,z=-26781..48729
+on x=-98497..-18186,y=-63569..3412,z=1232..88485
+on x=-726..56291,y=-62629..13224,z=18033..85226
+on x=-110886..-34664,y=-81338..-8658,z=8914..63723
+on x=-55829..24974,y=-16897..54165,z=-121762..-28058
+on x=-65152..-11147,y=22489..91432,z=-58782..1780
+on x=-120100..-32970,y=-46592..27473,z=-11695..61039
+on x=-18631..37533,y=-124565..-50804,z=-35667..28308
+on x=-57817..18248,y=49321..117703,z=5745..55881
+on x=14781..98692,y=-1341..70827,z=15753..70151
+on x=-34419..55919,y=-19626..40991,z=39015..114138
+on x=-60785..11593,y=-56135..2999,z=-95368..-26915
+on x=-32178..58085,y=17647..101866,z=-91405..-8878
+on x=-53655..12091,y=50097..105568,z=-75335..-4862
+on x=-111166..-40997,y=-71714..2688,z=5609..50954
+on x=-16602..70118,y=-98693..-44401,z=5197..76897
+on x=16383..101554,y=4615..83635,z=-44907..18747
+off x=-95822..-15171,y=-19987..48940,z=10804..104439
+on x=-89813..-14614,y=16069..88491,z=-3297..45228
+on x=41075..99376,y=-20427..49978,z=-52012..13762
+on x=-21330..50085,y=-17944..62733,z=-112280..-30197
+on x=-16478..35915,y=36008..118594,z=-7885..47086
+off x=-98156..-27851,y=-49952..43171,z=-99005..-8456
+off x=2032..69770,y=-71013..4824,z=7471..94418
+on x=43670..120875,y=-42068..12382,z=-24787..38892
+off x=37514..111226,y=-45862..25743,z=-16714..54663
+off x=25699..97951,y=-30668..59918,z=-15349..69697
+off x=-44271..17935,y=-9516..60759,z=49131..112598
+on x=-61695..-5813,y=40978..94975,z=8655..80240
+off x=-101086..-9439,y=-7088..67543,z=33935..83858
+off x=18020..114017,y=-48931..32606,z=21474..89843
+off x=-77139..10506,y=-89994..-18797,z=-80..59318
+off x=8476..79288,y=-75520..11602,z=-96624..-24783
+on x=-47488..-1262,y=24338..100707,z=16292..72967
+off x=-84341..13987,y=2429..92914,z=-90671..-1318
+off x=-37810..49457,y=-71013..-7894,z=-105357..-13188
+off x=-27365..46395,y=31009..98017,z=15428..76570
+off x=-70369..-16548,y=22648..78696,z=-1892..86821
+on x=-53470..21291,y=-120233..-33476,z=-44150..38147
+off x=-93533..-4276,y=-16170..68771,z=-104985..-24507)";
 char const* pData = R"(on x=-33..18,y=-35..11,z=-49..2
 on x=-14..32,y=5..49,z=-42..5
 on x=-28..18,y=-38..10,z=-14..33
