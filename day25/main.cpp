f82fcb76c7a2e6ddaa235d0ee6a4c8d051f56fb6
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2021-12-25 13:12:15 +0100

    day25 part 1 ok

--- day25/main.cpp
+++ day25/main.cpp
@@ -41,6 +41,65 @@ struct Pos {
 };
 using Transform = std::map<Pos, Pos>;
 
+Result solve(OceanFloor& current_floor) {
+  Result result{ 0 };
+  auto max_row = current_floor.size() - 1;
+  auto max_col = current_floor[0].size() - 1;
+
+  std::pair<Transform, Transform> next_transform{};
+  int step_count{ 0 };
+  do {
+    next_transform = {};
+    // transform position of '>'
+    for (size_t row = 0; row <= max_row; row++) {
+      for (size_t col = 0; col <= max_col; col++) {
+        if (current_floor[row][col] == '>') {
+          auto next_col = (col == max_col) ? 0 : col + 1; // Wrap around
+          if (current_floor[row][next_col] == '.') {
+            next_transform.first[Pos{ row, col }] = { row,next_col };
+          }
+        }
+      }
+    }
+    // Transform position of 'v'
+    for (size_t row = 0; row <= max_row; row++) {
+      for (size_t col = 0; col <= max_col; col++) {
+        if (current_floor[row][col] == 'v') {
+          auto next_row = (row == max_row) ? 0 : row + 1; // Wrap around
+          if (current_floor[next_row][col] == '.') {
+            // may be moved-to from the left >?
+            auto prev_col = (col == 0) ? max_col : col - 1;
+            if (next_transform.first.contains(Pos{ next_row,prev_col }) == false) {
+              next_transform.second[{row, col}] = { next_row,col };
+            }
+          }
+          else {
+            // May be moved from occupying > moving right?
+            if (next_transform.first.contains(Pos{ next_row,col }) == true) {
+              next_transform.second[{row, col}] = { next_row,col };
+            }
+          }
+        }
+      }
+    }
+    // Apply next transform
+    for (auto const& transform : { next_transform.first,next_transform.second }) {
+      for (auto const& me : transform) {
+        if (current_floor[me.second.row][me.second.col] == '.') {
+          current_floor[me.second.row][me.second.col] = current_floor[me.first.row][me.first.col];
+          current_floor[me.first.row][me.first.col] = '.';
+        }
+        else {
+          std::cout << "\nERROR - Move failed. Not free {row:" << me.second.row << ".col:" << me.second.col << "}";
+        }
+      }
+    }
+    step_count++;
+  } while (next_transform.first.size() > 0 or next_transform.second.size() > 0);
+  result = step_count;
+  return result;
+}
+
 namespace part1 {
   Result solve_for(std::string const file_name) {
       Result result{};
@@ -66,8 +125,13 @@ namespace part1 {
       auto current_floor = puzzle_model.front();
       auto max_row = current_floor.size() - 1;
       auto max_col = current_floor[0].size() - 1;
-      if (puzzle_model.size() > 1) {
+      if (puzzle_model.size() == 1) {
+        // Actual puzzle
+        result = solve(puzzle_model.front());
+      }
+      else if (puzzle_model.size() > 1) {
         // We have a sequence of example floors to test against 
+        int ex_floor_index{0};
 
         // Expect the number of possible moves are much smaller than the map size?
         // Operate on a set of moves instead of the actual map?
@@ -140,16 +204,45 @@ namespace part1 {
               std::cout << "\n" << row;
             }
           }
-          if (step_count >= puzzle_model.size()) {
-            std::cout << "\nFAILED - Exhausted test input";
-            break;
-          }
-          else if (current_floor == puzzle_model[step_count]) {
-            std::cout << "\npassed";
-          }
-          else {
-            std::cout << "\nFAILED";
-            break;
+          {
+            bool example_expected{ false };
+            switch (step_count) {
+              case 1:
+              case 2:
+              case 3:
+              case 4:
+              case 5: example_expected = true;  break;
+              case 10: example_expected = true; break;
+              case 20: example_expected = true; break;
+              case 30: example_expected = true; break;
+              case 40: example_expected = true; break;
+              case 50: example_expected = true; break;
+              case 55: example_expected = true; break;
+              case 56: example_expected = true; break;
+              case 57: example_expected = true; break;
+              case 58: example_expected = true; break;
+            }
+            if (example_expected) {
+              ex_floor_index++;
+              if (ex_floor_index >= puzzle_model.size()) {
+                std::cout << "\nFAILED - Exhausted test input at count " << ex_floor_index;
+                break;
+              }
+              else if (current_floor == puzzle_model[ex_floor_index]) {
+                std::cout << "\npassed";
+              }
+              else {
+                std::cout << "\nFAILED";
+                for (size_t row = 0; row <= max_row; row++) {
+                  for (size_t col = 0; col <= max_col; col++) {
+                    if (current_floor[row][col] != puzzle_model[ex_floor_index][row][col]) {
+                      std::cout << "\n\t{row:" << row << ",col:" << col << "} != " << puzzle_model[ex_floor_index][row][col];
+                    }
+                  }
+                }
+                break;
+              }
+            }
           }
         } while (next_transform.first.size()>0 or next_transform.second.size()>0);
       }
@@ -171,10 +264,8 @@ int main(int argc, char *argv[])
 {
   Answers answers{};
   //answers.push_back({"Part 1 Test",part1::solve_for("day25.ex")});
-  answers.push_back({ "Part 1 Test",part1::solve_for("day25.ex2") });
-  // answers.push_back({"Part 1     ",part1::solve_for(pData)});
-  // answers.push_back({"Part 2 Test",part2::solve_for(pTest)});
-  // answers.push_back({"Part 2     ",part2::solve_for(pData)});
+  // answers.push_back({ "Part 1 Test",part1::solve_for("day25.ex2") });
+  answers.push_back({ "Part 1 Test",part1::solve_for("day25.in") });
   for (auto const& answer : answers) {
     std::cout << "\nanswer[" << answer.first << "] " << answer.second;
   }
